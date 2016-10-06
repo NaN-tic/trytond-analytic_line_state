f68094dc366a92c6bbc41a1b5597397a358b25ca
Guillem Barba <guillem@nan-tic.com>
2016-10-06 16:02:04 +0200

    Fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_analytic_line_state import suite
+try:
+    from trytond.modules.analytic_line_state.tests.test_analytic_line_state import suite
+except ImportError:
+    from .test_analytic_line_state import suite
+
+__all__ = ['suite']
